41965da9a116a7bcf9ff745eace42aedef4b8743
sanengineer <48153104+sanengineer@users.noreply.github.com>
2020-06-17 17:42:56 +0700

    Initial Commit

---
+++ CameraSpec/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraSpec
+//
+//  Created by metalbee on 6/17/20.
+//  Copyright © 2020 sanengineer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
